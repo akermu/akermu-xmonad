f0b094e9982bdb41362c7b34a3fab7d930732cbf
Lukas Fürmetz <fuermetz@mailbox.org>
2017-01-19 19:15:09 +0100

    Remove emacs task timer from xmobar

--- src/xmobar.hs
+++ src/xmobar.hs
@@ -21,11 +21,10 @@ Config { font = "xft:Inconsolata:bold:size=16"
          , Run BatteryP ["BAT0", "BAT1"] ["-t", "<icon=battery-full.xpm/> <left>"] 60
          , Run Brightness ["-t", "<icon=lightbulb.xpm/> <percent>", "--", "-D", "intel_backlight"] 10
          , Run Com "ponymix" ["get-volume"] "volume" 10
-         , Run Com "bash" ["-c", "emacsclient -e \"(progn (require 'org-clock) (if (org-clocking-p) (substring-no-properties (org-clock-get-clock-string)) \\\"\\\"))\" | tr -d '\"'"] "task" 60
          , Run Com "bash" ["-c", "if [ -e /run/openvpn-client@*.pid ]; then echo '<icon=lock-locked.xpm/>'; else echo '<icon=lock-unlocked.xpm/>'; fi"] "vpn" 60
          , Run Date "%a %d.%m.%y %H:%M" "date" 10
          ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% } %date% { %task% %enp0s25% %wlp3s0% %vpn% <icon=volume-high.xpm/> %volume% %bright% %battery%"
+       , template = "%StdinReader% } %date% { %enp0s25% %wlp3s0% %vpn% <icon=volume-high.xpm/> %volume% %bright% %battery%"
        }
